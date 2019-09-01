dd891d6c551f0634fd15aecd90527b8d4ef97030
Alexander Demin <avdemin9@yandex.ru>
2019-09-01 21:42:42 +0300

    Updates

--- src/blocks/validateInput/validateInput.js
+++
@@ -1,7 +0,0 @@
-/* Функции валидации ввода*/
-
-import validateForm from '../validateForm/validateForm';
-
-export default function validateInput(evt) {
-    validateForm(evt.target.parentElement);
-  }
