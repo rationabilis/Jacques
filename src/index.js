f75e3c3b4962cb4116b8cfda3a7161cd78adc784
Alexander Demin <avdemin9@yandex.ru>
2019-09-07 23:40:56 +0300

    Исправлены ошибки указанные koras

--- src/index.js
+++ src/index.js
@@ -1,19 +1,23 @@
 "use strict";
 
+/* import praktikantInfo from './jscripts/praktikantInfo'; */
 import './style.css';
-import Popup from './blocks/popup/popup';
-import validateData from './blocks/validateData/validateData';
-import validateInput from './blocks/validateInput/validateInput';
-import {cardList, newPlaceButton, userButton, userNameHeader, aboutUserParagraph} from './blocks/consts/consts';
-import Api from './blocks/api/api';
+import Popup from './jscripts/popup';
+import validateData from './jscripts/validateData';
+import validateInput from './jscripts/validateInput';
+import {cardList, newPlaceButton, userButton, userNameHeader, aboutUserParagraph} from './jscripts/consts';
+import Api from './jscripts/api';
 
-const api = new Api({
+
+const praktikantInfo = {
   baseUrl: 'https://praktikum.tk/cohort1',
   headers: {
     authorization: '97536aad-2615-4e27-b54c-91daa47b4fd5',
     'Content-Type': 'application/json'
   }
-});
+};
+
+const api = new Api(praktikantInfo);
 
 //Запуск прорисовки страницы
 api.getUserData();
@@ -26,19 +30,11 @@ newPlaceButton.addEventListener('click', function() {
 });
 
 /* Ввод данных пользователя */
-userButton.addEventListener('click', function() {
+userButton.addEventListener('click', userButtonClick);
 
-  const userPopupContent = `<div class="popup__content">
-  <img src="../images/close.svg" alt="" class="popup__close popup__close_user">
-  <h3 class="popup__title">Редактировать профиль</h3>
-  <form class="popup__form" name="user">
-      <input id="username" type="text" name="username" class="popup__input popup__input_type_user-name" placeholder="Имя">
-      <span id="error-username" class="error-message">Это обязательное поле</span>
-      <input id="aboutuser" type="text" name="aboutuser" class="popup__input popup__input_type_about-user" placeholder="О себе">
-      <span id="error-aboutuser" class="error-message">Это обязательное поле</span>
-      <button class="button popup__button" disabled>+</button>
-  </form>
-</div>`;
+function userButtonClick() {
+
+  const userPopupContent = userPopupContentTemplate();
   const user = new Popup(userPopupContent);
   const formUser = document.forms.user;
   const userName = document.querySelector('#username');
@@ -50,6 +46,7 @@ userButton.addEventListener('click', function() {
   aboutUser.value = aboutUserParagraph.textContent;
   validateData(userName);
   validateData(aboutUser);
+  
     /* Считываем данные профиля из формы*/
   userName.addEventListener('input', validateInput);
   aboutUser.addEventListener('input', validateInput);
@@ -57,17 +54,33 @@ userButton.addEventListener('click', function() {
 
     /* Принимает данные формы */
   formUser.addEventListener('submit', function (evt) {
-  evt.preventDefault();
-    api.editUserData(userName.value, aboutUser.value)
-    .then((res) => {
-      userNameHeader.textContent = res.name;
-      aboutUserParagraph.textContent = res.about;
-      user.close();
-    })
-
+    evt.preventDefault();
+      api.editUserData(userName.value, aboutUser.value) /* При выносе функции возникает ошибка userName not defined*/
+      .then((res) => {                                   /* из-за того, что форма ввода формируется при вызове */
+        userNameHeader.textContent = res.name;
+        aboutUserParagraph.textContent = res.about;
+        user.close();
+      })
+  
     });
+
   /* Закрывает форму ввода данных пользователя */
-  popupUserCloseButton.addEventListener('click', function() {user.close();});
-});
+  popupUserCloseButton.addEventListener('click', function() {user.close();});}
+
+function userPopupContentTemplate() {
+  return `<div class="popup__content">
+  <img src="../images/close.svg" alt="" class="popup__close popup__close_user">
+  <h3 class="popup__title">Редактировать профиль</h3>
+  <form class="popup__form" name="user">
+      <input id="username" type="text" name="username" class="popup__input popup__input_type_user-name" placeholder="Имя">
+      <span id="error-username" class="error-message">Это обязательное поле</span>
+      <input id="aboutuser" type="text" name="aboutuser" class="popup__input popup__input_type_about-user" placeholder="О себе">
+      <span id="error-aboutuser" class="error-message">Это обязательное поле</span>
+      <button class="button popup__button" disabled>+</button>
+  </form>
+</div>`;
+};
+
+
 
 
